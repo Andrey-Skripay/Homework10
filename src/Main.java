f12cbad845efdd9f1dd6e980a55696be488aabcf
Andrey.Skripay <lordktan@gmail.com>
2023-10-15 12:06:46 +0500

    Задача 2 и исправленная 1 задача

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,21 @@ public class Main {
     public static void main(String[] args) {
        // Задание 1
 
-        String firstName = "Андрей";
-        String middleName = "Скрипай";
-        String lastName = "Игоревич";
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
         String fullName =  middleName + " " + firstName + " " + lastName ;
 
         System.out.println("Ф. И. О. сотрудника - " + fullName);
+
+// Задача 2
+
+        String fullName1 = "Ivanov Ivan Ivanovich";
+        String fullNameUpperCase = fullName1.toUpperCase();
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета - " + fullNameUpperCase);
+
+
 
 
     }
