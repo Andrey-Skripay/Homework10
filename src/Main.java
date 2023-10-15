abd5d356a7581f10969f349c11a69ab782106433
Andrey.Skripay <lordktan@gmail.com>
2023-10-15 12:00:18 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // Задание 1
+
+        String firstName = "Андрей";
+        String middleName = "Скрипай";
+        String lastName = "Игоревич";
+        String fullName =  middleName + " " + firstName + " " + lastName ;
+
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+
+
     }
 }
